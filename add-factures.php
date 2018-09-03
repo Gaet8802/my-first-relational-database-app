81b509bb473d0ed0c36b178c308974189a7bbebf
Geoffrey <geoffrey.poelmans@gmail.com>
2018-09-03 10:51:05 +0200

    ajout php dans add facture

--- add-factures.php
+++ add-factures.php
@@ -1,3 +1,35 @@
+<?php
+
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=cogip;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+  die('Erreur : '.$e->getMessage());
+}
+
+function addFactures($value='')
+{
+	global $bdd;
+
+	$invoice_date = $_POST["invoice_date"];
+	$id_company = $_POST["id_company"];
+	$designation = $_POST["designation"];
+
+	if ($invoice_date != NULL AND $id_company != "Choose" AND $objet != "") {
+		$sql = $bdd->prepare('INSERT INTO invoices(invoice_date, id_company, designation) VALUES(:$invoice_date, :id_company, :designation)');
+		$sql->execute(array(
+			'invoice_date' => $invoice_date,
+			'id_company' => $id_company,
+			'designation' => $designation,
+		));
+		echo " <p> La facture a été ajoutée avec succès. </p>";
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,30 +43,28 @@
     <a href="">Retour à l'accueil</a>
     <h3>Création d'une facture</h3>
 	<form action="" method="post">
-		<div>
-			<label for="Numéro">Numéro de la facture</label>
-			<input type="text" name="name" value="">
+    <div>
+			<label for="invoice_date">Date de la facture</label>
+			<input type="date" name="invoice_date">
 		</div>
         <div>
-			<label for="Date">Date de la facture</label>
-			<input type="text" name="name" placeholder="00/00/00">
-		</div>
-        <div>
-			<label for="société">Société</label>
-			<select name="société">
-				<option value="">a </option>
-				<option value="">b </option>
-				<option value="">c </option>
-				<option value="">d </option>
-				<option value="">e </option>
+			<label for="id_company">Société</label>
+			<select name="id_company">
+				<option value="Choose">Choose</option>
+				<option value="a">a </option>
+				<option value="b">b </option>
+				<option value="c">c </option>
+				<option value="d">d </option>
+				<option value="e">e </option>
 			</select>
 		</div>
 	    <div>
-			<label for="Objet de la facture">Objet de la facture</label>
-			<input type="textarea" name="commentaires" value="">
+			<label for="designation">Objet de la facture</label>
+			<input type="textarea" name="designation" value="">
 		</div>
 	    </div>
 		<button type="submit" name="button">Envoyer</button>
 	</form>
+	<?php if (isset($_POST["button"])) addFactures(); ?>
 </body>
 </html>
---
+++ suppliers.php
@@ -0,0 +1,83 @@
+<?php
+
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=cogip;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+  die('Erreur : '.$e->getMessage());
+}
+
+function showSuppliers($value='')
+{
+  global $bdd;
+
+  echo "
+    <tr>
+     <th>Company name</th>
+     <th>Company address</th>
+     <th>Country</th>
+     <th>VAT number</th>
+     <th>Company phone</th>
+		 <th>Company type</th>
+    </tr>
+  ";
+
+  $reponse = $bdd->prepare('SELECT * FROM company');
+  $reponse ->execute();
+
+  foreach($reponse as $donnees)
+  {
+    echo "
+      <tr>
+        <td>" . $donnees['company_name'] . "</td>
+        <td>" . $donnees['company_address'] . "</td>
+        <td>" . $donnees['country'] . "</td>
+        <td>" . $donnees['VAT_number'] . "</td>
+				<td>" . $donnees['company_phone'] . "</td>
+				<td>" . $donnees['company_type'] . "</td>
+        <td>
+          <form class='' action='' method='post'>
+						<input type='submit' name='submitShow' value='Show'>
+						<input type='hidden' name='show' value='".$donnees['id']."'>
+						<input type='hidden' name='hiddenPage' value='customers.php'>
+					</form>
+          <form class='' action='update-customers.php' method='post'>
+						<input type='submit' name='submitEdit' value='Edit'>
+						<input type='hidden' name='edit' value='".$donnees['id']."'>
+						<input type='hidden' name='hiddenPage' value='customers.php'>
+          </form>
+          <form class='' action='' method='post'>
+						<input type='submit' name='submitDelete' value='Delete'>
+						<input type='hidden' name='delete' value='".$donnees['id']."'>
+						<input type='hidden' name='hiddenPage' value='customers.php'>
+          </form>
+        </td>
+      </tr>
+    ";
+  }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Customers</title>
+  </head>
+  <body>
+    <h1>Your Customers</h1>
+    <h3>Customers</h3>
+    <a href="#">Index</a>
+    <a href="#">Suppliers</a>
+    <a href="#">Customers</a>
+    <form class="" action="annuaire.php" method="post">
+			<input type="submit" name="" value="Add customers">
+    </form>
+    <table>
+	  	<?php showSuppliers(); ?>
+    </table>
+  </body>
+</html>
