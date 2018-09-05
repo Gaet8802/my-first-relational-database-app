1edf667869f148540555e2e5184682a8e7af4096
guillaume <guillaume@guillaumes-MacBook-Pro-2.local>
2018-09-05 15:41:49 +0200

    accueil bootstrap

--- accueil.php
+++ accueil.php
@@ -29,12 +29,15 @@ require 'DBconnect.php';
 	        die('Erreur : '.$e->getMessage());
 	}
 	
-	$resultat = $bdd->query('SELECT * FROM invoices ORDER BY invoice_date ASC LIMIT 5');
+	$resultat = $bdd->query('SELECT Company.company_name,Invoices.customer_name,Invoices.invoice_date,Invoices.designation
+							from Company,Invoices
+							where Invoices.id_company = Company.id 
+							ORDER BY invoice_date ASC LIMIT 5');
 	?>
 
-
-	<table class="table table-bordered">
-	    <thead class ="thead-dark">
+	<h1 class="mt-2 d-flex justify-content-center display-4">vos cinq dernières factures</h1>
+	<table class="table table-bordered mt-4">
+	    <thead>
 	        <tr>
 	            <th scope="col">Nom de l'entreprise</th>
 	            <th scope="col">Nom du client</th>
@@ -47,7 +50,7 @@ require 'DBconnect.php';
 	    echo
 	        '<tbody>
 	            <tr>
-	                <td>'.$donnees['id_company'].'</td>
+	                <td>'.$donnees['company_name'].'</td>
 	                <td>'.$donnees['customer_name'].'</td>
 	                <td>'.$donnees['invoice_date'].'</td>
 	                <td>'.$donnees['designation'].'</td>
@@ -56,8 +59,9 @@ require 'DBconnect.php';
 	    ;}
 	?>
 	</table>
+	<h1 class="mt-2 d-flex justify-content-center display-4">cinq dernières personnes encodées</h1>
 	<table class="table table-bordered">
-	    <thead class="thead-dark">
+	    <thead>
 	        <tr>
 	            <th scope="col">Nom</th>
 	            <th scope="col">Prénom</th>
@@ -67,7 +71,7 @@ require 'DBconnect.php';
 	    </thead>
 	<?php        
 	
-	$resultat = $bdd->query('SELECT * FROM customers ORDER BY Customer_number');
+	$resultat = $bdd->query('SELECT * FROM customers ORDER BY Customer_number DESC LIMIT 5');
 	
 	while ($donnees= $resultat->fetch()){
 	    echo 
@@ -82,6 +86,7 @@ require 'DBconnect.php';
 	;}
 	?>
 	</table>
+	<h1 class="mt-2 d-flex justify-content-center display-4">cinq dernières sociétés encodées</h1>
 	<table class="table table-bordered">
 	    <thead>
 	        <tr>
@@ -97,7 +102,7 @@ require 'DBconnect.php';
 	<?php
 	
 	$resultat = $bdd->prepare('select Company.id,Company.company_name,Company.company_address,Company.country,Company.VAT_number,Company.company_phone,Company_Type.type_name
-	from Company,Company_Type where Company.company_type = company_Type.id');
+	from Company,Company_Type where Company.company_type = company_Type.id order by Company.id desc limit 5');
 	$resultat -> execute();
 	
 	foreach($resultat as $donnees){
@@ -121,19 +126,19 @@ require 'DBconnect.php';
 	                '</td>
 	                <td>
 	                <form class="" action="route_company.php" method="post">
-	                                <input type="submit" name="submitShow" value="Show">
-	                                <input type="hidden" name="show" value="'.$donnees['id'].'">
-	                                <input type="hidden" name="hiddenPage" value="customers.php">
+	                                <input type="submit" class="btn btn-warning m-1 text-white" name="submitShow" value="Show">
+	                                <input type="hidden" class="btn btn-warning m-1 text-white" name="show" value="'.$donnees['id'].'">
+	                                <input type="hidden" class="btn btn-warning m-1 text-white" name="hiddenPage" value="customers.php">
 	                </form>
 	                <form class="" action="update-customers.php" method="post">
-	                                <input type="submit" name="submitEdit" value="Edit">
-	                                <input type="hidden" name="edit" value="'.$donnees['id'].'">
-	                                <input type="hidden" name="hiddenPage" value="customers.php">
+	                                <input type="submit" class="btn btn-warning m-1 text-white" name="submitEdit" value="Edit">
+	                                <input type="hidden" class="btn btn-warning m-1 text-white" name="edit" value="'.$donnees['id'].'">
+	                                <input type="hidden" class="btn btn-warning m-1 text-white" name="hiddenPage" value="customers.php">
 	                </form>
 	                <form class="" action="" method="post">
-	                                <input type="submit" name="submitDelete" value="Delete">
-	                                <input type="hidden" name="delete" value="'.$donnees['id'].'">
-	                                <input type="hidden" name="hiddenPage" value="customers.php">
+	                                <input type="submit" class="btn btn-warning m-1 text-white" name="submitDelete" value="Delete">
+	                                <input type="hidden" class="btn btn-warning m-1 text-white" name="delete" value="'.$donnees['id'].'">
+	                                <input type="hidden" class="btn btn-warning m-1 text-white" name="hiddenPage" value="customers.php">
 	                </form>
 	                </td>
 	                </tr>
