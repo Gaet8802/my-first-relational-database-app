155afd5aec4cf51f2a258930fc93dd231e2326d1
Geoffrey <geoffrey.poelmans@gmail.com>
2018-09-05 11:25:24 +0200

    ajouts bootstrap

--- header.php
+++ header.php
@@ -1,40 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        <div class="container-fluid">
+<header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm">
+                <img class="logo" src="COGIP.png" alt="COGIP-LOGO">
+            </div>
+            <div class="col-sm d-flex justify-content-center align-items-center">
+                <h1>BONJOUR ....!</h1>
+            </div>
+            <div class="col-sm d-flex justify-content-end">
+                <a href="logout.php">Deconnexion</a>
+            </div>
+        </div>
             <div class="row">
                 <div class="col-sm">
-                    <img class="logo" src="COGIP.png" alt="COGIP-LOGO">
-                </div>
-                <div class="col-sm d-flex justify-content-center align-items-center">
-                    <h1>BONJOUR ....!</h1>
-                </div>
-                <div class="col-sm d-flex justify-content-end">
-                    <a href="logout.php">Deconnexion</a>
-                </div>
-            </div>
-                <div class="row">
-                    <div class="col-sm">
-                        <button onclick="location.href='accueil.php'" type="button" class="btn btn-dark">Accueil</button>
-                        <button onclick="location.href='suppliers.php'" type="button" class="btn btn-dark">Fournisseurs</button>
-                        <button onclick="location.href='customers.php'" type="button" class="btn btn-dark">Clients</button>
-                        <button onclick="location.href='invoices.php'" type="button" class="btn btn-dark">Factures</button>
-                        <button onclick="location.href='annuaire.php'" type="button" class="btn btn-dark">Contact</button>
-                    </div>
+                    <button onclick="location.href='accueil.php'" type="button" class="btn btn-dark">Accueil</button>
+                    <button onclick="location.href='suppliers.php'" type="button" class="btn btn-dark">Fournisseurs</button>
+                    <button onclick="location.href='customers.php'" type="button" class="btn btn-dark">Clients</button>
+                    <button onclick="location.href='invoices.php'" type="button" class="btn btn-dark">Factures</button>
+                    <button onclick="location.href='annuaire.php'" type="button" class="btn btn-dark">Contact</button>
                 </div>
             </div>
         </div>
-    </header>
-</body>
-</html>
+    </div>
+</header>
