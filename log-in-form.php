da5d355304fffc5a05417630143bad066e75be0d
Geoffrey <geoffrey.poelmans@gmail.com>
2018-09-05 15:32:14 +0200

    modif log in form

--- log-in-form.php
+++ log-in-form.php
@@ -18,14 +18,14 @@ catch(Exception $e)
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <title>Login</title>
+    <title>COGIP</title>
   </head>
   <body>
     <div id="main">
-      <h1>PHP Login Session</h1>
+      <h1>COGIP</h1>
     </div>
     <div id="login">
-      <h2>Login Form</h2>
+      <h2>Login</h2>
     </div>
     <form action="login.php" method="post">
       <label>UserName :</label>
